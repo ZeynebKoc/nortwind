2b9d80c241031d15503b3e0668ffebfc862bf048
Zeyneb Koç <koczeyneb@gmail.com>
2022-01-24 20:04:39 +0300

    Bind CategoryList Component to database

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
 
 class CategoryList extends Component {
+    componentDidMount() {
+        this.props.action.getCategories();
+    }
     render() {
         return (
             <div>
                 <h1>Category List</h1>
-                <h3>
-                   {this.props.currentCategory.categoryName}
-                </h3>
+                <ListGroup>
+                    {this.props.categories.map((category) => (
+                        <ListGroupItem key={category.id}>
+                            {category.categoryName}
+                        </ListGroupItem>
+                    ))}
+                </ListGroup>
+                <h3>{this.props.currentCategory.categoryName}</h3>
             </div>
         );
     }
@@ -17,7 +28,19 @@ class CategoryList extends Component {
 function mapStateToProps(state) {
     return {
         currentCategory: state.changeCategoryReducer,
+        categories: state.categoryListReducer,
     };
 }
 
-export default connect(mapStateToProps)(CategoryList);
+function mapDispatchToProps(dispatch) {
+    return {
+        action: {
+            getCategories: bindActionCreators(
+                categoryActions.getCategories,
+                dispatch
+            ),
+        },
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
