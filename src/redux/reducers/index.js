b682ba293987b2f971aef224c7d97b0c56d5e5d4
Zeyneb Koç <koczeyneb@gmail.com>
2022-01-25 15:41:44 +0300

    Creat action and reducer for ProductList

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import changeCategoryReducer from "./changeCategoryReducer";
 import categoryListReducer from "./categoryListReducer";
+import changeProductReducer from "./changeProductReducer";
 
 const rootReducer = combineReducers({
     changeCategoryReducer,
     categoryListReducer,
+    changeProductReducer,
 });
 export default rootReducer;
