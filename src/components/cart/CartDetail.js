b952771197d20a1f8d68ddb50bed4d5a24d675bc
Zeyneb Koç <koczeyneb@gmail.com>
2022-01-26 19:55:56 +0300

    Create CartDetail Component and router

---
+++ src/components/cart/CartDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CartDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Demo</h3>
+            </div>
+        );
+    }
+}
--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
 import Navi from "../navi/Navi";
 import Dashboard from "./Dashboard";
+import CartDetail from "../cart/CartDetail";
 
 function App() {
     return (
         <div>
             <Container>
                 <Navi />
-                <Dashboard />
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route path="/product" component={Dashboard} />
+                    <Route path="/cart" component={CartDetail} />
+                </Switch>
             </Container>
         </div>
     );
