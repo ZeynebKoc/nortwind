83695ff20d2b3dced81ba0a47b02b5265e4b0516
Zeyneb Koç <koczeyneb@gmail.com>
2022-01-31 20:07:35 +0300

    Add SelectInput Component to ProductDetail Component

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TextInput from "../toolbox/TextInput";
+import SelectInput from "../toolbox/SelectInput";
 
 const ProductDetail = ({ categories, product, onSave, onChange }) => {
     return (
@@ -12,6 +13,19 @@ const ProductDetail = ({ categories, product, onSave, onChange }) => {
                 onChange={onChange}
                 error="Error"
             />
+            <SelectInput
+                name="categoryId"
+                label="Category"
+                value={product.categoryId || ""}
+                defaultOption="Choose"
+                options={categories.map((category) => ({
+                    value: category.id,
+                    text: category.categoryName,
+                }))}
+                onChange={onChange}
+                error="Error"
+            />
+
             <button type="submit" className="btn btn-success">
                 Save
             </button>
