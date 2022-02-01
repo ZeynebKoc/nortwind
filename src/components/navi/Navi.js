ef12da668cdeb8e42cad730eb1fdb08e2a081026
Zeyneb Koç <koczeyneb@gmail.com>
2022-02-01 16:51:09 +0300

    Link elements in Navi Component

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -10,24 +10,22 @@ import {
     NavbarText,
 } from "reactstrap";
 import CartSummary from "../cart/CartSummary";
+import { Link } from "react-router-dom";
 
 export default class Navi extends Component {
     render() {
         return (
             <div>
                 <Navbar color="light" expand="md" light>
-                    <NavbarBrand href="/">Northwind</NavbarBrand>
+                    <NavbarBrand>
+                        <Link to="/">Northwind Store</Link>
+                    </NavbarBrand>
                     <NavbarToggler onClick={function noRefCheck() {}} />
                     <Collapse navbar>
                         <Nav className="me-auto" navbar>
                             <NavItem>
-                                <NavLink href="/components/">
-                                    Components
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">
-                                    GitHub
+                                <NavLink>
+                                    <Link to="/saveproduct">Add Product</Link>
                                 </NavLink>
                             </NavItem>
                             <CartSummary />
